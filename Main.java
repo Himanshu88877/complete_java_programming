0268e5f8bd225abd91ccfb1acefc8a5d1498bdee
HIMANSHU <146114835+Himanshu88877@users.noreply.github.com>
2025-06-21 00:40:59 +0530

    Add files via upload
    day-01

---
+++ Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello and welcome!");
+        System.out.println("declaring variable");
+        int num=4;
+        // two type of data type 1.primitive datatype 2.non=-primitive datatype
+        // primitive data type are - byte, short, int, float, double, long, char,boolean
+        // non primitive data type--derivd from primitive data types .
+        int a=2;
+        int b=3;
+        int c=6;
+        System.out.println(a+b+c);
+//        literal in java any constant value assign to a variable is called literals
+        byte a1=67;
+        short a2=87;
+        int a3= 78;
+        long a4=789564797L;
+        float a5=7.8f;
+        double a6=6.98;
+        char a7='A';
+        boolean a8=true;
+        System.out.println(a1);
+        }
+    }
