33f3d8f2e02c71bcd6b2b4c28a747c0120256f5e
HIMANSHU <146114835+Himanshu88877@users.noreply.github.com>
2025-06-24 23:53:08 +0530

    Add files via upload

---
+++ DY_06.java
@@ -0,0 +1,19 @@
+public class DY_06 {
+    public static void main(String[] args) {
+        System.out.println("precedance and associtivity  of operators");
+//        precedance--operator applied nd evaluated on the basis of precedane (+,-) has less precedance then (*,/).
+//        associtivity--tell the direction of excution it can from L to R or R to L.
+
+        int a =6*8 - 16/2;
+//        48-8
+//         40   precedance is applied
+        System.out.println(a);
+
+        int b= 16/2-2*2;
+//        left to right
+//        8-4=4
+        System.out.println(b);
+  // iske quize silve karna hai bhai
+
+    }
+}
